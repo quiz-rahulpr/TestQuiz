41704c95984a216dd9d30a9cc902243116d7916d
quiz-rahulpr <okbie584@gmail.com>
2026-01-06 12:43:27 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Lisbon", correct: false }
+        ]
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        answers: [
+            { text: "Earth", correct: false },
+            { text: "Mars", correct: true },
+            { text: "Jupiter", correct: false },
+            { text: "Saturn", correct: false }
+        ]
+    },
+    {
+        question: "What is 2 + 2?",
+        answers: [
+            { text: "3", correct: false },
+            { text: "4", correct: true },
+            { text: "5", correct: false },
+            { text: "22", correct: false }
+        ]
+    },
+    {
+        question: "Which runs in a web browser?",
+        answers: [
+            { text: "Java", correct: false },
+            { text: "C", correct: false },
+            { text: "Python", correct: false },
+            { text: "JavaScript", correct: true }
+        ]
+    }
+];
